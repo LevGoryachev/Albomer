2388397636fc79a02d9bac7259c076601c9fe63b
Lev <l.goryachev@inbox.ru>
2020-04-20 17:09:47 +0300

    First commit

---
+++ Albomer/src/main/java/ru/goryachev/albomer/App.java
@@ -0,0 +1,13 @@
+package ru.goryachev.albomer;
+
+/**
+ * Albomer - app for creating file album (version 1.0.1)
+ */
+public class App {
+    
+	public static void main( String[] args )    {
+       
+    	
+    	
+    }
+}
