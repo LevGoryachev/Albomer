a4c03ac2d16314d18f2f4885c943fe4b847321a6
Lev <l.goryachev@inbox.ru>
2020-04-26 02:03:29 +0300

    addHyperlinkListener instead of addMouseListener

--- Albomer/src/main/java/ru/goryachev/albomer/Editor.java
+++ Albomer/src/main/java/ru/goryachev/albomer/Editor.java
@@ -4,14 +4,13 @@ import java.awt.Component;
 import java.awt.Container;
 import java.awt.Desktop;
 import java.awt.Dimension;
-import java.awt.Point;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.dnd.DnDConstants;
 import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetDropEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -25,19 +24,15 @@ import javax.swing.SpringLayout;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
 
+
 public class Editor extends JFrame implements HyperlinkListener {
 
 		private JTextField nameField;
 		private JTextField descField;
-		private JTextField numberField;
 		private JEditorPane editorField;
 		private String txtInField = "";
-		private String txtClick = "";
-				
-		private JButton renamerButton;
-		
-		Desktop desk = Desktop.getDesktop();
-		
+						
+		private JButton saveButton;
 			
 	public Editor() {
 	
@@ -60,23 +55,17 @@ public class Editor extends JFrame implements HyperlinkListener {
 			this.editorField = new JEditorPane();
 			editorField.setPreferredSize(new Dimension (750, 300));
 			editorField.setContentType("text/html");
-			
 			Pane.add(editorField);
 			
 			JScrollPane jspArea = new JScrollPane(editorField);
 			Pane.add(jspArea);
-			
+						
 		//Tips
 			JTextPane tips = new JTextPane();
 			tips.setEditable(false);
 			tips.setContentType("text/html");
-			tips.setText("<b><font color=\"#556B2F\">В поле можно вводить текст, но обязательно отделять пробелами от файлов</font></b>");
+			tips.setText("<b><font color=\"#556B2F\">В поле пока нельзя вводить текст</font></b>");
 			Pane.add(tips);			
-			
-			//hyperPane.setContentType("text/html");
-					
-			//hyperPane.setText("Ispytanie hyperssylky:<a href='mailto:michael@uml.com'>e-mail to</a> or do smth else");
-			
 		//
 										
 			editorField.setDropTarget(new DropTarget() {
@@ -88,12 +77,10 @@ public class Editor extends JFrame implements HyperlinkListener {
 			            
 			            for (File file : droppedFiles) {
 			            							            				
-			            	txtInField = txtInField + "<p>" + "<strong>" + "<a href=" + "file://localhost/" + file.toString() +">" + file.toString() + "</a>" + "</strong>" + "<font color='green'>" + " - " + "</font>" +  "</p>";
+			            	txtInField = txtInField + "<p>" + "<strong>" + "<a href=" + "file:/" + file.toString() +">" + file.toString() + "</a>" + "</strong>" + "<font color='green'>" + " - " + "</font>" +  "</p>";
 			            				            				            	
 			            	editorField.setText(txtInField);
-			            	
-			            	txtClick = txtClick + file.toString() + " ";
-			            	
+			            				            	
 			            	System.out.println(txtInField);
 			            }
 			            
@@ -103,59 +90,29 @@ public class Editor extends JFrame implements HyperlinkListener {
 			    }
 			});
 		
-			editorField.addMouseListener(new MouseAdapter() {
+			
+			editorField.setEnabled(true);
+			editorField.setEditable(false);
+			
+			editorField.addHyperlinkListener(new HyperlinkListener() {
+			    public void hyperlinkUpdate(HyperlinkEvent e) {
+			        if(e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+			            if(Desktop.isDesktopSupported()) {
+			        	    try {
+								//Desktop.getDesktop().browse(e.getURL().toURI()); // for http
+								Desktop.getDesktop().open(new File(e.getURL().toURI()));
+								
+							} catch (IOException | URISyntaxException e1) {
+								e1.printStackTrace();
+							}
+			        	}
+			        }
+			    }
+			});
 				
-                public void mouseClicked(MouseEvent me) {
-                	
-                	//double click
-                    if(me.getClickCount()==2) {
-                    
-                        int x = me.getX();
-                        int y = me.getY();
-
-                        int startOffset = editorField.viewToModel(new Point(x, y));//where on jtextarea click was made
-                        
-                        //String text = editorField.getText();
-                        String text = txtClick;
-                        
-                        int searchAdress = 0;
-                        int wordEndIndex = 0;
-                        String[] words = text.split("\\s");//spliting the text to words. link will be a single word
-
-                        for(String word:words) {
-                        	
-                        	//find the resource link
-                            if(word.startsWith("https://") || word.contains(":")) {
-                            	searchAdress = text.indexOf(word);
-                                wordEndIndex = searchAdress+word.length();
-                                
-                                if(startOffset>=searchAdress && startOffset<=wordEndIndex) {
-                                    try {
-                                    	
-                                    	editorField.select(searchAdress, wordEndIndex);
-                                    	                                    	
-                                    	desk.getDesktop().open(new File(word));
-                                	
-                                    	
-                                    }
-                                    catch(Exception e)
-                                    {
-                                        e.printStackTrace();
-                                    }
-
-                                }
-                            }
-                        }                           
-                    }
-
-                }
-            });
-      
-			
-			
-			this.renamerButton = new JButton("Сохранить");
-			Pane.add(renamerButton);
-			//renamerButton.addActionListener(btnClick);
+			this.saveButton = new JButton("Сохранить");
+			Pane.add(saveButton);
+			//saveButton.addActionListener(btnClick);
 			
 			layout.putConstraint(SpringLayout.WEST , nameLabel, 10, SpringLayout.WEST , Pane);
 			layout.putConstraint(SpringLayout.NORTH, nameLabel, 25, SpringLayout.NORTH, Pane);
@@ -172,11 +129,11 @@ public class Editor extends JFrame implements HyperlinkListener {
 			layout.putConstraint(SpringLayout.NORTH, jspArea, 35, SpringLayout.NORTH, fileLabel);
 			layout.putConstraint(SpringLayout.WEST , jspArea, 50, SpringLayout.WEST , Pane);
 						
-			layout.putConstraint(SpringLayout.WEST , tips, 200, SpringLayout.WEST , Pane);
+			layout.putConstraint(SpringLayout.WEST , tips, 300, SpringLayout.WEST , Pane);
 			layout.putConstraint(SpringLayout.NORTH, tips, 10, SpringLayout.SOUTH, jspArea);	
 			
-			layout.putConstraint(SpringLayout.WEST , renamerButton, 375, SpringLayout.WEST , Pane);
-			layout.putConstraint(SpringLayout.NORTH, renamerButton, 25, SpringLayout.SOUTH, tips);	
+			layout.putConstraint(SpringLayout.WEST , saveButton, 375, SpringLayout.WEST , Pane);
+			layout.putConstraint(SpringLayout.NORTH, saveButton, 25, SpringLayout.SOUTH, tips);	
 			
 			setLocation(250, 300);
 			setSize(850, 600);
