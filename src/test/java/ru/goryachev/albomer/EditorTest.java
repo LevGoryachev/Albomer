20414840e4da07b2eec216bd716103335c8fe043
LevGoryachev <l.goryachev@inbox.ru>
2020-08-05 17:13:00 +0300

    JUnit: parametrized EditorTest.java

--- src/test/java/ru/goryachev/albomer/EditorTest.java
+++ src/test/java/ru/goryachev/albomer/EditorTest.java
@@ -2,32 +2,43 @@ package ru.goryachev.albomer;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RunWith(Parameterized.class)
 public class EditorTest {
 
     public String savedName;
     public String savedDesc;
     public String savedField;
+    public Boolean expected;
+
+    public EditorTest(String savedName, String savedDesc, String savedField, Boolean expected) {
+        this.savedName = savedName;
+        this.savedDesc = savedDesc;
+        this.savedField = savedField;
+        this.expected = expected;
+    }
 
     @Test
     public void testEditor() {
         Editor editor = new Editor(savedName, savedDesc, savedField);
 
-        List<String> expected = new ArrayList<>();
-        expected.add(savedName);
-        expected.add(savedDesc);
-        expected.add(savedField);
-
-        List<String> actual = new ArrayList<>();
-        actual.add(editor.getName());
-        actual.add(editor.getDesc());
-        actual.add(editor.getTxtInField());
+        Boolean actual = editor.getName() == savedName && editor.getDesc() == savedDesc && editor.getTxtInField() == savedField;
 
         Assert.assertNotNull(editor);
         Assert.assertEquals(expected, actual);
+    }
 
+    @Parameterized.Parameters
+    public static List<Object[]> testData() {
+        List<Object[]> params = new ArrayList<>();
+        params.add(new Object[] {"AlbomOne", "Description one", "Fields list one", true});
+        params.add(new Object[] {" ", " ", " ", true});
+        params.add(new Object[] {null, null, null, true});
+    return params;
     }
 }
